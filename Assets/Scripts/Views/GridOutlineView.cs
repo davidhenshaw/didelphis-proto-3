647bfe9c6c00b3fa1a6a64d7302933d8a76ae2a6
David Henshaw <davidhenshaw2014@gmail.com>
2024-04-27 16:03:34 -0500

    better outline generation algorithm (still probably rough)

--- Assets/Scripts/Views/GridOutlineView.cs
+++ Assets/Scripts/Views/GridOutlineView.cs
@@ -35,88 +35,134 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
         if (!_grid.Equals(model.grid))
             return;
 
-        var positions = GetVerticesFromCells(model.highlightedCells);
+        var positions = GetPerimeterVertices(model.highlightedCells);
+
+        if (positions == null)
+            return;
 
         _lineRenderer.positionCount = positions.Length;
         _lineRenderer.SetPositions(positions);
     }
 
-    private Vector3[] GetVerticesFromCells(Vector3Int[] cells)
+    /// <summary>
+    /// Returns true if each cell shares at least one face with another cell
+    /// </summary>
+    /// <param name="cells"></param>
+    /// <returns></returns>
+    private bool IsContiguous(ICollection<Vector3Int> cells)
     {
-        Dictionary<Vector3, int> vertexRepetitions = new Dictionary<Vector3, int>();
-        SortedSet<Vector3> perimeter = new SortedSet<Vector3>();
-        Vector3 avgPosition = Vector3.zero;
+        foreach(var cell in cells)
+        {
+            bool sharesFace = cells.Contains(cell + Vector3Int.up) ||
+                cells.Contains(cell + Vector3Int.down) ||
+                cells.Contains(cell + Vector3Int.right) ||
+                cells.Contains(cell + Vector3Int.left);
 
+            if (!sharesFace)
+                return false;
+        }
+
+        return true;
+    }
+    /// <summary>
+    /// Given an array of <paramref name="cells"/>, returns the perimeter vertices in an order where a line can be drawn through them. <br/>
+    /// Returns null if the cells are not contiguous
+    /// </summary>
+    /// <param name="cells"></param>
+    /// <returns></returns>
+    private Vector3[] GetPerimeterVertices(Vector3Int[] cells)
+    {
+        if (!IsContiguous(cells))
+        {
+            Debug.LogWarning("Group of cells is not contiguous and will not be rendered.");
+            return null;
+        }
+
+        Dictionary<Vector3, int> vertexCounts = new Dictionary<Vector3, int>();
+        float xOffset = _grid.cellSize.x * 0.5f;
+        float yOffset = _grid.cellSize.y * 0.5f;
+
+        ///Keep track of how many times we generate the same vertices
         void CountVertex(Vector3 vertex)
         {
-            if(vertexRepetitions.ContainsKey(vertex))
+            if(vertexCounts.ContainsKey(vertex))
             {
-                vertexRepetitions[vertex]++;
+                if(vertexCounts[vertex]++ >= 4) //if a vertex shares 4 cells, it is internal to the shape
+                {
+                    vertexCounts.Remove(vertex); // We only care about perimeter vertices, so remove it
+                }
             }
             else
             {
-                vertexRepetitions.Add(vertex, 1);
+                vertexCounts.Add(vertex, 1);
             }
         }
 
         foreach(var cell in cells)
         {
-            float xOffset = _grid.cellSize.x * 0.5f;
-            float yOffset = _grid.cellSize.y * 0.5f;
             var pointNW = _grid.GetCellCenterLocal(cell) + new Vector3(xOffset * -1, yOffset, 0);
             var pointNE = _grid.GetCellCenterLocal(cell) + new Vector3(xOffset, yOffset, 0);
             var pointSW = _grid.GetCellCenterLocal(cell) + new Vector3(xOffset * -1, yOffset * -1, 0);
             var pointSE = _grid.GetCellCenterLocal(cell) + new Vector3(xOffset, yOffset * -1, 0);
 
+            pointNW.z = pointNE.z = pointSW.z = pointSE.z = 0;
 
             CountVertex(pointNW);
             CountVertex(pointNE);
             CountVertex(pointSE);
             CountVertex(pointSW);
 
-            avgPosition += pointNE + pointNW + pointSE + pointSW;
         }
+        
+        List<Vector3> orderedList = new();
+        List<Vector3> remainingVertices = new List<Vector3>(vertexCounts.Keys);
+        Vector3 currVertex = Vector3.zero;
 
-        //Points on the perimeter will only repeat less than 4 times. If the same vertex is counted 4 times, it is internal to the shape
-        //Get average position of PERIMETER points only.
-        avgPosition /= vertexRepetitions.Count((kvp) => {return kvp.Value < 4; });
-
-        ClockwiseComparer comparer = new ClockwiseComparer(avgPosition);
-        perimeter = new SortedSet<Vector3>(comparer);
-
-
-        foreach(var vertex in vertexRepetitions.Keys)
+        while(HasNext(remainingVertices, currVertex, _grid.cellSize, out Vector3 nextVertex))
         {
-            if (vertexRepetitions[vertex] >= 4)
-                continue;
-            perimeter.Add(vertex);
+            orderedList.Add(currVertex);
+
+            remainingVertices.Remove(nextVertex);
+            currVertex = nextVertex;
         }
 
-        return perimeter.ToArray();
+        return orderedList.ToArray();
     }
 
-    private class ClockwiseComparer : IComparer<Vector3>
+    /// <summary>
+    /// Checks in <paramref name="vertices"/> for a vertex above, right, left, and below exactly <paramref name="stepSize"/> away as the <paramref name="nextVertex"/>
+    /// </summary>
+    /// <param name="vertices"></param>
+    /// <param name="currVertex"></param>
+    /// <param name="stepSize"></param>
+    /// <param name="nextVertex"></param>
+    /// <returns></returns>
+    private bool HasNext(List<Vector3> vertices, Vector3 currVertex, Vector2 stepSize, out Vector3 nextVertex)
     {
-        public Vector3 pivot;
-
-        public ClockwiseComparer(Vector3 averagePoint)
+        nextVertex = Vector3.zero;
+        if (vertices.Count == 0)
         {
-            this.pivot = averagePoint;
-            pivot.z = 0;
+            return false;
         }
 
-        public int Compare(Vector3 v1, Vector3 v2)
-        {
-            v1.z = 0;
-            v2.z = 0;
-            //Get the vector between each point and the average position
-            var v1pivot = v1 - pivot;
-            var v2pivot = v2 - pivot;
+        //Check for adjacent vertices in clockwise order
+        Vector3[] nextChecks = {
+            currVertex + new Vector3(0, stepSize.y), //up
+            currVertex + new Vector3(stepSize.x, 0), //right
+            currVertex + new Vector3(0, -1 * stepSize.y), //down
+            currVertex + new Vector3( -1 *stepSize.x, 0) //left
+        };
 
-            var v1angle = Vector3.SignedAngle(v1pivot, Vector3.down, Vector3.forward);
-            var v2angle = Vector3.SignedAngle(v2pivot, Vector3.down, Vector3.forward);
-            return Mathf.CeilToInt(v1angle - v2angle);
+        for (int i = 0; i < nextChecks.Length; i++)
+        {
+            if (vertices.Contains(nextChecks[i] ))
+            {
+                nextVertex = nextChecks[i];
+                return true;
+            }
         }
+
+        return false;
     }
 }
 
