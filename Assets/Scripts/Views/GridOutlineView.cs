458ffbc42729b15106f0349e345fe21bf9632c6e
David Henshaw <davidhenshaw2014@gmail.com>
2024-04-28 09:08:02 -0500

    big update to outline algorithm

--- Assets/Scripts/Views/GridOutlineView.cs
+++ Assets/Scripts/Views/GridOutlineView.cs
@@ -51,6 +51,9 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
     /// <returns></returns>
     private bool IsContiguous(ICollection<Vector3Int> cells)
     {
+        if (cells.Count <= 1)
+            return true;
+
         foreach(var cell in cells)
         {
             bool sharesFace = cells.Contains(cell + Vector3Int.up) ||
@@ -78,25 +81,30 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
             return null;
         }
 
-        Dictionary<Vector3, int> vertexCounts = new Dictionary<Vector3, int>();
+        Dictionary<Vector3, HashSet<Vector3Int>> vertexMap = new Dictionary<Vector3, HashSet<Vector3Int>>();
         float xOffset = _grid.cellSize.x * 0.5f;
         float yOffset = _grid.cellSize.y * 0.5f;
 
         ///Keep track of how many times we generate the same vertices
-        void CountVertex(Vector3 vertex)
+        void CountVertex(Vector3 vertex, Vector3Int cell)
         {
-            if(vertexCounts.ContainsKey(vertex))
+            if(vertexMap.ContainsKey(vertex))
             {
-                if(vertexCounts[vertex]++ >= 4) //if a vertex shares 4 cells, it is internal to the shape
+                vertexMap[vertex].Add(cell);
+                if(vertexMap[vertex].Count >= 4) //if a vertex shares 4 cells, it is internal to the shape
                 {
-                    vertexCounts.Remove(vertex); // We only care about perimeter vertices, so remove it
+                    vertexMap.Remove(vertex); // We only care about perimeter vertices, so remove it
                 }
             }
             else
             {
-                vertexCounts.Add(vertex, 1);
+                vertexMap.Add(vertex, new HashSet<Vector3Int>{ cell });
             }
         }
+
+        //vertex we will start traversal with
+        // must be guaranteed to be on external edge of shape
+        Vector3 startVertex = Vector3.zero;
 
         foreach(var cell in cells)
         {
@@ -107,20 +115,24 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
 
             pointNW.z = pointNE.z = pointSW.z = pointSE.z = 0;
 
-            CountVertex(pointNW);
-            CountVertex(pointNE);
-            CountVertex(pointSE);
-            CountVertex(pointSW);
+            CountVertex(pointNW, cell);
+            CountVertex(pointNE, cell);
+            CountVertex(pointSE, cell);
+            CountVertex(pointSW, cell);
 
+            //start vertex is the most northwestern point we can find on the shape
+            startVertex = (pointNW.x < startVertex.x) && (pointNW.y < startVertex.y) ? pointNW : startVertex;
         }
         
         List<Vector3> orderedList = new();
-        List<Vector3> remainingVertices = new List<Vector3>(vertexCounts.Keys);
-        Vector3 currVertex = Vector3.zero;
+        List<Vector3> remainingVertices = new List<Vector3>(vertexMap.Keys);
+        Vector3 currVertex = startVertex;
+        orderedList.Add(startVertex);
+        remainingVertices.Remove(startVertex);
 
-        while(HasNext(remainingVertices, currVertex, _grid.cellSize, out Vector3 nextVertex))
+        while(HasNext(remainingVertices, vertexMap,currVertex, _grid.cellSize, out Vector3 nextVertex))
         {
-            orderedList.Add(currVertex);
+            orderedList.Add(nextVertex);
 
             remainingVertices.Remove(nextVertex);
             currVertex = nextVertex;
@@ -130,14 +142,14 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
     }
 
     /// <summary>
-    /// Checks in <paramref name="vertices"/> for a vertex above, right, left, and below exactly <paramref name="stepSize"/> away as the <paramref name="nextVertex"/>
+    /// Checks in <paramref name="vertices"/> for a vertex above, right, left, or below exactly <paramref name="stepSize"/> away as the <paramref name="nextVertex"/>
     /// </summary>
     /// <param name="vertices"></param>
     /// <param name="currVertex"></param>
     /// <param name="stepSize"></param>
     /// <param name="nextVertex"></param>
     /// <returns></returns>
-    private bool HasNext(List<Vector3> vertices, Vector3 currVertex, Vector2 stepSize, out Vector3 nextVertex)
+    private bool HasNext(List<Vector3> vertices, Dictionary<Vector3, HashSet<Vector3Int>> vertexMap,Vector3 currVertex, Vector2 stepSize, out Vector3 nextVertex)
     {
         nextVertex = Vector3.zero;
         if (vertices.Count == 0)
@@ -157,12 +169,35 @@ public class GridOutlineView : MonoBehaviour, IView<GridOutlineModel>
         {
             if (vertices.Contains(nextChecks[i] ))
             {
+                if (!IsJumpValid(currVertex, nextChecks[i], vertexMap))
+                    continue;
+
                 nextVertex = nextChecks[i];
                 return true;
             }
         }
 
         return false;
+    }
+
+    /// <summary>
+    /// For jumps to be valid, <strong>exactly one cell</strong> must be common to the <paramref name="vOrigin"/> and <paramref name="vOther"/> vertex<br/>
+    /// Otherwise, the jump would either pass through the shape or jump across vertices which shouldn't be connected
+    /// </summary>
+    /// <param name="vOrigin"></param>
+    /// <param name="vOther"></param>
+    /// <param name="vertexMap"></param>
+    /// <returns></returns>
+    private bool IsJumpValid(Vector3 vOrigin, Vector3 vOther, Dictionary<Vector3, HashSet<Vector3Int>> vertexMap)
+    {
+        var originCells = new HashSet<Vector3Int>(vertexMap[vOrigin]);
+        var otherCells = new HashSet<Vector3Int>(vertexMap[vOther]);
+        //Look don't ask how you figured this out but just be glad you did
+
+        //For jumps to be valid, exactly one cell must be common to the origin vertex
+
+        originCells.IntersectWith(otherCells);
+        return originCells.Count == 1;
     }
 }
 
